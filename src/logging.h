d92e3b2f000e23117d04416ff88bd1df5fdeee2f
thebeastxdxd <idoba12@gmail.com>
2021-02-21 18:20:08 +0200

    added some comments

--- src/logging.h
+++ src/logging.h
@@ -4,6 +4,8 @@
 #include "printf.h"
 #include "vga.h"
 
+// __VA_OPT__ is used here to allow the macro to not get any parameters except format
+// __VA_ARGS__ is a macro to open up the arguments gotten
 #define LOG(format, ...)                                                       \
     printf("%s:%d:%s():" format "\n ", __FILE__, (int)__LINE__, __func__ __VA_OPT__(,)      \
            __VA_ARGS__)
